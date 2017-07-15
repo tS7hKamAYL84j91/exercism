402d6c04b78ee884e69a4c98decea2218e0b83fd
tS7hKamAYL84j91 <jim@jimbob-air.local>
2017-07-15 21:52:38 +0100

    scabble_scores with macro

--- elixir/scrabble-score/scrabble.exs
+++ elixir/scrabble-score/scrabble.exs
@@ -1,33 +1,34 @@
+defmodule ScrabbleScoreMaker do
+  defmacro letter_scores(scores) do
+    scores 
+    |> Enum.map(fn {k,v} -> Enum.map(k, &{&1,v})  end) 
+    |> List.flatten 
+    |> Enum.map(fn {letter, score} -> 
+        quote do
+          def letter_score(unquote(letter)), do: unquote(score)
+        end
+      end)
+    |> Kernel.++([(quote do  def letter_score(_), do: 0 end)])
+  end
+end
+
 defmodule Scrabble do
-  #require ScrabbleScoreMaker
+  require ScrabbleScoreMaker
   @doc """
   Calculate the scrabble score for the word.
   """
-  @letter_scores %{
-    ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T" ] => 1,
-    ["D","G"] => 2,
-    ["B", "C", "M", "P"] => 3,
-    ["F", "H", "V", "W", "Y" ] => 4,
-    ["K"] => 5,
-    ["J", "X"] => 8,
-    ["Q", "Z"] => 10
-  }
-
+  ScrabbleScoreMaker.letter_scores([
+    {["A", "E", "I", "O", "U", "L", "N", "R", "S", "T" ], 1},
+    {["D","G"], 2},
+    {["B", "C", "M", "P"] , 3},
+    {["F", "H", "V", "W", "Y" ], 4},
+    {["K"], 5},
+    {["J", "X"], 8},
+    {["Q", "Z"], 10}
+  ])
 
   @spec score(String.t) :: non_neg_integer
-  def score(word) do
-    word 
-    |> String.upcase 
-    |> String.codepoints
-    |> Enum.map(&letter_score/1)
-    |> Enum.sum 
-  end
-
-  @spec letter_score(String.t) :: non_neg_integer
-  def letter_score(letter), do:  @letter_scores |> Map.get(letter |> letter_score_key, 0)
-  
-  @spec letter_score_key(String.t) :: [String.t]
-  def letter_score_key(letter), do: @letter_scores |> Map.keys |> Enum.map(&({letter in &1, &1})) |> Keyword.get(true)
+  def score(word),do:  word |> String.upcase |> String.codepoints |> Enum.map(&letter_score/1) |> Enum.sum 
 
 
 end
