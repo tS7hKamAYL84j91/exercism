3871db7fd430d150f3955abf8968364078c16b9c
tS7hKamAYL84j91 <jim@jimbob-air.local>
2017-07-15 21:29:47 +0100

    Scabble score maker interation 1

--- elixir/scrabble-score/scrabble.exs
+++ elixir/scrabble-score/scrabble.exs
@@ -1,9 +1,33 @@
 defmodule Scrabble do
+  #require ScrabbleScoreMaker
   @doc """
   Calculate the scrabble score for the word.
   """
+  @letter_scores %{
+    ["A", "E", "I", "O", "U", "L", "N", "R", "S", "T" ] => 1,
+    ["D","G"] => 2,
+    ["B", "C", "M", "P"] => 3,
+    ["F", "H", "V", "W", "Y" ] => 4,
+    ["K"] => 5,
+    ["J", "X"] => 8,
+    ["Q", "Z"] => 10
+  }
+
+
   @spec score(String.t) :: non_neg_integer
   def score(word) do
-
+    word 
+    |> String.upcase 
+    |> String.codepoints
+    |> Enum.map(&letter_score/1)
+    |> Enum.sum 
   end
+
+  @spec letter_score(String.t) :: non_neg_integer
+  def letter_score(letter), do:  @letter_scores |> Map.get(letter |> letter_score_key, 0)
+  
+  @spec letter_score_key(String.t) :: [String.t]
+  def letter_score_key(letter), do: @letter_scores |> Map.keys |> Enum.map(&({letter in &1, &1})) |> Keyword.get(true)
+
+
 end
--- elixir/sublist/sublist_performance.exs
+++ elixir/sublist/sublist_performance.exs
@@ -1,17 +1,7 @@
 defmodule Sublist do
   @doc """
   Returns whether the first list is a sublist or a superlist of the second list
-  and if not whether it is equal or unequal to the second list.
-
-  Huge test input was asking for some changes to try and make faster
-  Navie approach just use streams and make the comparison async in a new process
-  
-  Replaced member? with any? test
-  $ elixir sublist/sublist_test.exs  | grep huge
-    * test huge sublist not in huge list (1050.5ms) <- 1258 (1st iteration)
-    * test superlist early in huge list (165.2ms) <- 752
-    * test sublist early in huge list (120.6ms) <- 764
-
+  and if not whether it is equal or unequal to the second list
   """
   def compare(a,a), do: :equal # replaced a === b
   def compare(a,b) when length(a) > length(b), do: with :sublist <- compare(b,a), do: :superlist
