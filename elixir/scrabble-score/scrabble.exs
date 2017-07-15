1d89362f90bf868dae05f0c4a3baf5217b359957
tS7hKamAYL84j91 <jim@jimbob-air.local>
2017-07-15 23:12:11 +0100

    scrabble with functions being created by a generator

--- elixir/scrabble-score/scrabble.exs
+++ elixir/scrabble-score/scrabble.exs
@@ -1,34 +1,22 @@
-defmodule ScrabbleScoreMaker do
-  defmacro letter_scores(scores) do
-    scores 
-    |> Enum.map(fn {k,v} -> Enum.map(k, &{&1,v})  end) 
-    |> List.flatten 
-    |> Enum.map(fn {letter, score} -> 
-        quote do
-          def letter_score(unquote(letter)), do: unquote(score)
-        end
-      end)
-    |> Kernel.++([(quote do  def letter_score(_), do: 0 end)])
-  end
-end
-
 defmodule Scrabble do
-  require ScrabbleScoreMaker
   @doc """
   Calculate the scrabble score for the word.
   """
-  ScrabbleScoreMaker.letter_scores([
-    {["A", "E", "I", "O", "U", "L", "N", "R", "S", "T" ], 1},
+   @letter_score [{["A", "E", "I", "O", "U", "L", "N", "R", "S", "T" ], 1},
     {["D","G"], 2},
     {["B", "C", "M", "P"] , 3},
     {["F", "H", "V", "W", "Y" ], 4},
     {["K"], 5},
     {["J", "X"], 8},
-    {["Q", "Z"], 10}
-  ])
+    {["Q", "Z"], 10}]
 
   @spec score(String.t) :: non_neg_integer
-  def score(word),do:  word |> String.upcase |> String.codepoints |> Enum.map(&letter_score/1) |> Enum.sum 
-
+  def score(word), do: word |> String.upcase |> String.codepoints |> Enum.reduce(0,&(&1 |> letter_score |> Kernel.+(&2))) 
+  
+  for {letter, score} <- (for {k,v} <- @letter_score do  k |> Enum.map(&{&1,v}) end |> List.flatten) do
+    def letter_score(unquote(letter)), do: unquote(score)
+  end
+  
+  def letter_score(_), do: 0
 
 end
