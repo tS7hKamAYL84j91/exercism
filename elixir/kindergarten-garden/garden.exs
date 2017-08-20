acf2a1f93ed77a7aef212cc05f2679eddec2c637
tS7hKamAYL84j91 <jim@jimbob-air.local>
2017-08-20 09:18:04 +0100

    garden iteration #3

--- elixir/kindergarten-garden/garden.exs
+++ elixir/kindergarten-garden/garden.exs
@@ -1,4 +1,6 @@
 defmodule Garden do
+  @students  ~w/alice bob charlie david eve fred ginny harriet ileana joseph kincaid larry/a
+  @plants    %{"C" => :clover, "G" => :grass, "R" => :radishes, "V" => :violets}
   @doc """
     Accepts a string representing the arrangement of cups on a windowsill and a
     list with names of students in the class. The student names list does not
@@ -9,6 +11,20 @@ defmodule Garden do
   """
 
   @spec info(String.t(), list) :: map
-  def info(info_string, student_names) do
+  def info(info_string, student_names \\ @students) do
+    student_names 
+    |> Map.new(fn x -> {x, {}} end) # All students with no plants
+    |> Map.merge( # overlay students matched to plants
+      student_names
+      |> Enum.sort_by(&Atom.to_string/1)
+      |> Enum.zip(info_string |> parse_info |> Enum.map(&format_info/1)) # match plants to students
+      |> Enum.into(%{}))
   end
+
+  defp parse_info(info_string), do: info_string |> String.split("\n") |> Enum.map(&String.codepoints/1) |> group_plants 
+  
+  defp group_plants([[p1, p2| []], [p3, p4| []]]), do: [[p1, p2, p3, p4] ]
+  defp group_plants([[p1, p2| r1], [p3, p4| r2]]), do: [[p1, p2, p3, p4] | [r1, r2] |> group_plants]
+
+  defp format_info(plant_codes), do: plant_codes |> Enum.map(&(@plants[&1])) |> List.to_tuple
 end
