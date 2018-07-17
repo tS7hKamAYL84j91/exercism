eb350429f5db6ad6b78ecb02542a8e0f7a16f450
tS7hKamAYL84j91 <jimandsalbrown@gmail.com>
2018-07-17 09:06:47 +0100

    simples clock interation #2

--- elixir/clock/clock.exs
+++ elixir/clock/clock.exs
@@ -1,15 +1,13 @@
 defmodule Clock do
-  defstruct hour: 0, minute: 0
+  @mins_in_day 24 * 60
+
+  defstruct minutes: 0
 
   defimpl String.Chars, for: Clock do
-    def to_string(%Clock{hour: hh, minute: mm}),
-      do: "#{hh |> pad_leading_zero}:#{mm |> pad_leading_zero}"
+    def to_string(%Clock{minutes: mm}), do: "#{mm |> fmt_time(&div/2)}:#{mm |> fmt_time(&rem/2)}"
 
-    defp pad_leading_zero(i), do: i |> Kernel.to_string() |> String.pad_leading(2, "0")
-  end
-
-  defimpl Inspect, for: Clock do
-    def inspect(clock, _opts), do: clock |> to_string
+    defp fmt_time(mm, f) when is_function(f),
+      do: f.(mm, 60) |> Kernel.to_string() |> String.pad_leading(2, "0")
   end
 
   @doc """
@@ -19,16 +17,10 @@ defmodule Clock do
       "08:09"
   """
   @spec new(integer, integer) :: Clock
-  def new(hour, minute),
-    do: %Clock{hour: clock_hours(hour, minute), minute: clock_minutes(minute)}
+  def new(hh, mm), do: %Clock{minutes: (hh * 60 + mm) |> modulo_mins}
 
-  def clock_hours(hh, mm) when rem(mm, 60) < 0, do: clock_hours(hh - 1, div(mm, 60) * 60)
-  def clock_hours(hh, mm), do: rem(hh + div(mm, 60), 24) |> clock_hours
-  def clock_hours(hh) when hh < 0, do: 24 + hh
-  def clock_hours(hh), do: hh
-
-  defp clock_minutes(mm) when rem(mm, 60) < 0, do: 60 + rem(mm, 60)
-  defp clock_minutes(mm), do: rem(mm, 60)
+  defp modulo_mins(mm) when rem(mm, @mins_in_day) < 0, do: rem(mm, @mins_in_day) + @mins_in_day
+  defp modulo_mins(mm), do: rem(mm, @mins_in_day)
 
   @doc """
   Adds two clock times:
@@ -37,5 +29,5 @@ defmodule Clock do
       "10:03"
   """
   @spec add(Clock, integer) :: Clock
-  def add(%Clock{hour: hour, minute: minute}, add_minute), do: new(hour, minute + add_minute)
+  def add(%Clock{minutes: mm}, add_mm), do: new(0, mm + add_mm)
 end
