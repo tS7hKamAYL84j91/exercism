a9d6db4a5969e951666569021121754995a9568a
tS7hKamAYL84j91 <jimandsalbrown@gmail.com>
2019-06-01 00:13:09 +0100

    gains UInt64

--- julia/grains/grains.jl
+++ julia/grains/grains.jl
@@ -1,3 +1,3 @@
-apply(square, f) = 1 <= square <= 64 ? f(square) : throw(DomainError("oops"))
-on_square(square) =  apply(square, s->Int128(1) << (s - 1)) # replace with bit shift Int128(2)^(s - 1)) 
-total_after(square) = apply(square, s->(Int128(1) << s) - 1)  # replace with bit shift and some fancy maths mapfoldl(on_square, (+), 1:s))
+apply_to_valid(square, f) = 1 <= square <= 64 ? f(square) : throw(DomainError("oops"))
+on_square(square) =  apply_to_valid(square, s->UInt64(1) << (s - 1))        # replace with bit shift Int128(2)^(s - 1)) 
+total_after(square) = apply_to_valid(square, s->(UInt64(1) << s) - 1)       # replace with bit shift and some fancy maths mapfoldl(on_square, (+), 1:s))
