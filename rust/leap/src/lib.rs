890833ed627ec504b6240df530f76f4b43ef31a2
tS7hKamAYL84j91 <jimandsalbrown@gmail.com>
2019-10-11 08:38:05 +0100

    leap year rust

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,3 +1,9 @@
 pub fn is_leap_year(year: u64) -> bool {
-    unimplemented!("true if {} is a leap year", year)
+  let div_by = |year, n| year % n == 0;
+  match year {
+    year if div_by(year, 400) => true,
+    year if div_by(year, 100) => false,
+    year if div_by(year, 4) => true,
+    _ => false,
+  }
 }
