102b85b3332e2600ad17ac80eac247d098b4abbd
jim <jim@jimbob-air.local>
2021-03-28 12:26:13 +0100

    tinkering f#, swift, clojurescript, purescript, racket

---
+++ fsharp/queen-attack/QueenAttack.fs
@@ -0,0 +1,10 @@
+﻿module QueenAttack
+
+let create (row, col) =
+    let posIsValid (row) = (row >= 0 && row < 8)
+    posIsValid row && posIsValid col
+
+let canAttack (q1row, q1col) (q2row, q2col) =
+    q1row = q2row
+    || q1col = q2col
+    || abs (q2row - q1row) = abs (q2col - q1col)
---
+++ fsharp/raindrops/Raindrops.fs
@@ -0,0 +1,24 @@
+﻿module Raindrops
+
+let drops =
+    [ (3, "Pling")
+      (5, "Plang")
+      (7, "Plong") ]
+
+let is_divisible_by dividend divisor = (dividend % divisor) = 0
+
+let sound_of_drops number (drop, drop_sound) =
+    match is_divisible_by number drop with
+    | true -> drop_sound
+    | false -> ""
+
+let default_sound number rain =
+    match rain with
+    | "" -> number.ToString()
+    | rain -> rain
+
+let convert (number: int) : string =
+    drops
+    |> List.map (sound_of_drops number)
+    |> String.concat ""
+    |> default_sound number
