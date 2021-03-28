53d7c402ddb1710911d1efe079a7bf83221b5564
Jim Brown <jim@jimbob-air.local>
2021-03-28 12:22:19 +0100

    tinkering f#, racket, clojurescript, purescript & swift

---
+++ fsharp/two-fer/TwoFer.fs
@@ -0,0 +1,8 @@
+﻿module TwoFer
+
+let rec twoFer =
+    function
+    | Some null -> twoFer None // Seems that there is some crazy programmers that could send Some null :(
+    | Some name when name.Trim() = "" -> twoFer None
+    | Some name -> $"One for {name}, one for me."
+    | None -> "One for you, one for me."
