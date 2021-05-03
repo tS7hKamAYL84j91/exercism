0db46318b6dc6ad1fb6cceba55f7489d56ca6edb
jim <jim@jimbob-air.local>
2021-05-03 22:48:16 +0100

    Robot simulator in f#

---
+++ fsharp/kindergarten-garden/KindergartenGarden.fs
@@ -0,0 +1,32 @@
+﻿module KindergartenGarden
+
+type Plant =
+    | Grass
+    | Clover
+    | Radishes
+    | Violets
+    | None
+
+let plants (diagram: string) student =
+    let numberOfPlantsPerRow = 2
+
+    let studentIndex (student: string) = int (student.[0]) - int 'A'
+
+    let plantsInRow student (diagram: string) =
+        (studentIndex student) * numberOfPlantsPerRow
+        |> fun indx -> diagram.[indx..indx + 1]
+        |> Seq.toList
+
+    let plantType =
+        function
+        | 'G' -> Grass
+        | 'C' -> Clover
+        | 'R' -> Radishes
+        | 'V' -> Violets
+        | _ -> None
+
+    diagram.Split('\n')
+    |> Seq.toList
+    |> List.map (plantsInRow student)
+    |> List.concat
+    |> List.map plantType
---
+++ fsharp/robot-simulator/RobotSimulator.fs
@@ -0,0 +1,49 @@
+﻿module RobotSimulator
+
+type Direction =
+    | North
+    | East
+    | South
+    | West
+
+type Position = int * int
+
+type Robot =
+    { direction: Direction
+      position: Position }
+
+let (+) ((x1, y1): Position) ((x2, y2): Position) = (x1 + x2, y1 + y2)
+
+let create direction position =
+    { direction = direction
+      position = position }
+
+let move instructions robot =
+    let advance state =
+        match state.direction with
+        | North -> (0, 1)
+        | South -> (0, -1)
+        | East -> (1, 0)
+        | West -> (-1, 0)
+        |> fun pos ->
+            { state with
+                  position = state.position + pos }
+
+    let turnLeft state =
+        match state.direction with
+        | North -> West
+        | South -> East
+        | East -> North
+        | West -> South
+        |> fun dir -> { state with direction = dir }
+
+    let turnRight = turnLeft >> turnLeft >> turnLeft
+
+    let execute state cmd =
+        match cmd with
+        | 'L' -> turnLeft state
+        | 'R' -> turnRight state
+        | 'A' -> advance state
+        | _ -> state
+
+    Seq.fold execute robot instructions
