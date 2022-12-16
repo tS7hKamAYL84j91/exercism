76ece283323e449ab66f08bddbf531c357ee633e
Jim <jimandsalbrown@gmail.com>
2022-12-16 15:19:08 +0000

    fsharp Lucians Lasagna

---
+++ fsharp/lucians-luscious-lasagna/LuciansLusciousLasagna.fs
@@ -0,0 +1,7 @@
+module LuciansLusciousLasagna
+
+let expectedMinutesInOven = 40
+let remainingMinutesInOven minutesInOven = expectedMinutesInOven - minutesInOven
+let preparationTimeInMinutes layers = layers * 2
+let elapsedTimeInMinutes layers minutesInOven = 
+  preparationTimeInMinutes layers + minutesInOven
